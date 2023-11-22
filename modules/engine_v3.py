5febd1cbda061d4a6f294e19deb40576d6ea833c
parlanti <146109171+parlanti@users.noreply.github.com>
2023-11-22 12:32:57 +0100

    [UPD] Added RQ3

--- modules/engine_v3.py
+++ modules/engine_v3.py
@@ -9,63 +9,76 @@ from . import engine_v1
 
 df_original = pd.DataFrame()
 
-path_courses_matrix_tf_idf = "data/courses_matrix_tf_idf.csv"
-path_inverted_index_tf_idf = 'data/inverted_index_tf_idf.json'
+path_courses_matrix_new_score = "data/courses_matrix_new_score.csv"
+path_inverted_index_new_score = 'data/inverted_index_new_score.json'
 path_norms = 'data/norms.csv'
 
 
-def compute_tf_idf() -> pd.DataFrame:
+def compute_score() -> pd.DataFrame:
     """
-    Computes the tf-idf for each word in the in each document (i.e. course),
-    where tf-idf = tf * idf = term frequency * inverse document frequency.
+    Computes the score for each word in the in each document (i.e. course),
+    with the formula 
+        Score = TF_{courseName} + (1 + log(TF_{description})) * IDF_{description}
     Return the correspondent dataframe
     Returns:
-        tf_idf: The tf-idf dataframe
+        new_score: The score dataframe
     """
     # First of all we get the vocabulary using the engine_v1 module
     vocabulary = engine_v1.get_vocabulary()
     if vocabulary is None:
         return None
-
-    # Create a Scikit-learn TfidfVectorizer object
-    tfidf_vec = TfidfVectorizer(input='content', lowercase = False, tokenizer = lambda text: text, vocabulary = vocabulary)
     
     # Load the dataset
     global df_original
     df = df_original
     
-    # Transforming the 'prep_description' column into a tf-idf matrix
+    # Create a Scikit-learn TfidfVectorizer object
+    # With the Sublinear option we apply the formula 1 + log(tf) instead of tf
+    # In this way we penalise the words that appear a lot of times in a document
+    tfidf_vec = TfidfVectorizer(input='content', lowercase = False, tokenizer = lambda text: text, vocabulary = vocabulary, sublinear_tf = True)
+    
+    # Transforming the 'prep_description' column into a log-tf-idf matrix
     # and converting it into a dataframe
     results = tfidf_vec.fit_transform(df['prep_description'])
     result_dense = results.todense()
     tf_idf = pd.DataFrame(result_dense.tolist(), index = df.index, columns=list(vocabulary.keys()).sort())
     
-    # Save the tf-idf dataframe in the file courses_matrix_tf_idf.csv
-    with open(path_courses_matrix_tf_idf, "w") as f:
-        tf_idf.to_csv(f)
+    # Then compute the tf of the 'courseName' column
+    tfidf_vec2 = TfidfVectorizer(input='content', lowercase = False, tokenizer = lambda text: text, vocabulary = vocabulary, use_idf = False)
+    results2 = tfidf_vec2.fit_transform(df['courseName'].apply(engine_v1.preprocess))
+    result_dense2 = results2.todense()
+    tf_idf2 = pd.DataFrame(result_dense2.tolist(), index = df.index, columns=list(vocabulary.keys()).sort())
+        
+    # Sum the score of the 'courseName' and the score of the 'description'
+    new_score = tf_idf + tf_idf2    
+    
+        
+    # Save the score dataframe in the file courses_matrix_new_score.csv
+    with open(path_courses_matrix_new_score, "w") as f:
+        new_score.to_csv(f)
         
     # Here we compute the l2 norms for each document
-    tf_idf['norm'] = np.linalg.norm(tf_idf.values, axis = 1)
+    new_score['norm'] = np.linalg.norm(new_score.values, axis = 1)
          
     # We also save in the file norms.csv the norm of each document
     with open(path_norms, "w") as f:
-        tf_idf['norm'].to_csv(f)
+        new_score['norm'].to_csv(f)
     
-    return tf_idf
+    return new_score
 
 
-def get_tf_idf() -> pd.DataFrame:
+def get_score() -> pd.DataFrame:
     """
-    This function loads the tf-idf dataframe from the courses_matrix_tf_idf.csv file
+    This function loads the score dataframe from the courses_matrix_new_score.csv file
     Args:
         df (pd.DataFrame): The dataframe containing the columns 'index' and 'description'
     Returns:
-        tf_idf: The tf-idf dataframe
+        df_score: The score dataframe
     """
     try:
-        with open(path_courses_matrix_tf_idf, "r") as f:
-            tf_idf = pd.read_csv(f, index_col = 'index')
-        return tf_idf
+        with open(path_courses_matrix_new_score, "r") as f:
+            df_score = pd.read_csv(f, index_col = 'index')
+        return df_score
     except Exception as e:
         print("Tf-Idf index has not been computed yet")
         return None
@@ -88,18 +101,18 @@ def get_norms() -> pd.DataFrame:
 
 def create_inverted_index(df: pd.DataFrame) -> dict:
     """
-    Computes the inverted tf-idf index for each word in the vocabulary.
+    Computes the inverted score index for each word in the vocabulary.
     Returns a dictionary of the format:
     {
-      term_id_1:[(document1, tfIdf_{term,document1}), (document2, tfIdf_{term,document2}), (document4, tfIdf_{term,document4}), ...],
-      term_id_2:[(document2, tfIdf_{term,document1}), (document6, tfIdf_{term,document6}), ...],
+      term_id_1:[(document1, score_{term,document1}), (document2, score_{term,document2}), (document4, score_{term,document4}), ...],
+      term_id_2:[(document2, score_{term,document1}), (document6, score_{term,document6}), ...],
         ...
     }
-    The dictionary is also saved in the file inverted_index_tf_idf.json for a faster access to it.
+    The dictionary is also saved in the file inverted_index_new_score.json for a faster access to it.
     Args:
         df (pd.DataFrame): The document dataframe
     Returns:
-        dict: The inverted tf-idf index
+        dict: The inverted score index
     """
     
     # Saving the original dataframe
@@ -117,7 +130,7 @@ def create_inverted_index(df: pd.DataFrame) -> dict:
         return None
     
     # First of all get the tf-idf index score 
-    tf_idf = compute_tf_idf()
+    df_score = compute_score()
 
     # Then we compute the inverted index
     for word, term_id in vocabulary.items():
@@ -125,13 +138,13 @@ def create_inverted_index(df: pd.DataFrame) -> dict:
         # Retrive the list of tuples where tf_idf > 0
         # to_records() is a pandas function that returns a list of tuples from a dataframe
         # tuple_list = pd.DataFrame(tf_idf[tf_idf[word] > 0][word]).to_records(index = True).tolist()
-        tuple_list = pd.DataFrame(tf_idf[tf_idf[term_id] > 0][term_id]).to_records(index = True).tolist()
+        tuple_list = pd.DataFrame(df_score[df_score[term_id] > 0][term_id]).to_records(index = True).tolist()
         
         # We add the list of tuples to the inverted index dictionary
         inverted_index[term_id] = tuple_list    
         
-    # Finally we save the inverted index dictionary in the file inverted_index_tf_idf.json
-    with open(path_inverted_index_tf_idf, "w") as f:
+    # Finally we save the inverted index dictionary in the file inverted_index_new_score.json
+    with open(path_inverted_index_new_score, "w") as f:
         json.dump(inverted_index, f)
         
     return inverted_index
@@ -139,13 +152,13 @@ def create_inverted_index(df: pd.DataFrame) -> dict:
 
 def get_inverted_index() -> dict:
     """
-    This function loads the inverted index from the inverted_index.json file
+    This function loads the inverted index from the inverted_index_new_score.json file
     If the inverted index file does not exists it returns None
     Returns:
         dict: The inverted index
     """
     try:
-        with open(path_inverted_index_tf_idf, "r") as f:
+        with open(path_inverted_index_new_score, "r") as f:
             inverted_index = json.load(f)
             
         # Converting the keys from string to int, since we have to use them as integers
@@ -159,8 +172,9 @@ def get_inverted_index() -> dict:
 def search(query: str, k: int = 10) -> list:
     """
     For each document we compute the cosine similarity with the query
-    using the tf-idf score previously evaluated
-    It returns a Heap with the k most similar documents and 
+    using the score 
+        TF_{courseName} + (1 + log(TF_{description})) * IDF_{description}
+    previously evaluated. It returns a Heap with the k most similar documents and 
     the relative similarity score
     Args:  
         words (list): The list of words in the query
@@ -172,13 +186,11 @@ def search(query: str, k: int = 10) -> list:
     # Preprocess the query
     words = engine_v1.preprocess(query)
     
-    
     # Get only the documents that contain all the words in the query
     # recycling the code from the search function of the engine_v1 module
     df_result = engine_v1.search(query)
     
-    
-    # Read the inverted index from the file inverted_index_tf_idf.json
+    # Read the inverted index from the file inverted_index_new_score.json
     inverted_index = get_inverted_index() 
     if inverted_index is None:
         print("Inverted index has not been computed yet")
@@ -225,7 +237,6 @@ def search(query: str, k: int = 10) -> list:
     # Fill nan's with 0
     df_tmp.fillna(0, inplace = True)
     
-    
     # Evaluate the query tf-idf score 'manually' without using the function tfidf_vectorizer.transform() of sklearn
     doc_frequencies = [(df_tmp[engine_v1.get_word_from_id(word_id)] != 0).sum() for word_id in query_words_ids]
     N = df_original.shape[0]
@@ -247,7 +258,7 @@ def search(query: str, k: int = 10) -> list:
     df_results['Similarity'].fillna(0, inplace = True)
     df_results = df_results[df_results['Similarity'] > 0]
     df_results = df_results.nlargest(k, 'Similarity')
-    
+        
     # Finally we create a heap structure to store the k most similary documents
     # to the query and we return its
     heap = []
